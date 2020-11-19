82992e16f3a7faea66c0b55670fe5beebd54cb45
S.Yabunaka[strcode] <yabunaka@strcode.biz>
2020-11-20 05:14:50 +0900

    Update main.js and style.css. - Added method Window size fixed. And style.css reorganized.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,22 @@
       
 {
   const chara = document.querySelector('#vtuber');
-  
+  const windowWidth = 639 / 2 + 20;
+  const windowHeight = 899.3 / 2 + 20;
+
   const recognition = new webkitSpeechRecognition();
   
+  window.resizeTo( windowWidth, windowHeight);
+
   recognition.lang = 'ja-JP';
   
-  recognition.onend = (event) => {
+  recognition.onend = event => {
     recognition.start();
   }
-  recognition.onspeechstart = (event) => {
+  recognition.onspeechstart = event => {
     chara.classList.add('talk');
   }
-  recognition.onspeechend = (event) => {
+  recognition.onspeechend = event => {
     chara.classList.remove('talk');
   }
   
